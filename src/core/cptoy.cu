5d84d6a8e08a1bb4c89bde09d5afe205087237d4
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-10-08 11:36:37 +0200

    compile in build

--- src/core/cptoy.cu
+++ src/core/cptoy.cu
@@ -5,12 +5,14 @@
 CPToy::CPToy(int size) :
     size(size), data(nullptr)
 {
+    MPI_Init(nullptr, nullptr);
     cudaMalloc(&data, sizeof(float) * size);
 }
 
 CPToy::~CPToy()
 {
     cudaFree(data);
+    MPI_Finalize();
 }
 
 __global__ void kernel_fill(float val, int n, float *data)
@@ -20,15 +22,22 @@ __global__ void kernel_fill(float val, int n, float *data)
         data[i] = val;
 }
 
-void cuda_test(float value) {
+void CPToy::cuda_test(float value) {
 
     const int nthreads = 128;
-    const int nblocks = (n + nthreads - 1) / nthreads;
+    const int nblocks = (size + nthreads - 1) / nthreads;
 
     kernel_fill <<<nblocks, nthreads>>> (value, size, data);
 }
 
-void mpi_test(float value) {
+void CPToy::mpi_test(float value) {
+    int rank, root = 0;
     float sum = 0;
-    // TODO
+
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Reduce(&value, &sum, 1, MPI_FLOAT, MPI_SUM, root, MPI_COMM_WORLD);
+
+    if (rank == root)
+        printf("sum of values: %g\n", sum);
 }
